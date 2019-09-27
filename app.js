a6b9c3afedcbd3257d829eb57c111425ccf67f75
daniel-0705 <lsps60711@hotmail>
2019-09-27 19:46:10 +0800

    把搜尋地址跟點選地名直接出現紅色地標並可以收藏 完成

--- app.js
+++ app.js
@@ -422,29 +422,24 @@ app.post("/api/user/map_list/place",async function(req,res){
     let select_user_result = await dao_map.select("user","access_token",list_data.data.access_token)
     if(select_user_result == 0){
       var error = {
-        "error": "There is no user data."
+        "error": "!無此使用者資訊，請登入或重新註冊"
       };
       res.send(error);
       return;
     }
 
     if(list_data.type == "create"){
-        
+      
       let user_list_data = {
         user_name : select_user_result[0].name,
         list_name : list_data.data.list_name,
         place_name :list_data.data.place_name,
-        place_order :list_data.data.place_order,
-        longitude : list_data.data.longitude,
-        latitude : list_data.data.latitude,
-        information :list_data.data.information
+        place_order :list_data.data.place_order
       };
 
-      console.log(user_list_data)
-      
       let select_user_insert_place = await dao_map.select_3("user_map_place","user_name",user_list_data.user_name,"place_name",user_list_data.place_name,"list_name",user_list_data.list_name);
       
-      console.log(select_user_insert_place)
+      console.log(select_user_insert_place);
       
       if(select_user_insert_place.length >0){
         var error = {
@@ -454,12 +449,22 @@ app.post("/api/user/map_list/place",async function(req,res){
         return;
       }
 
+      //將目標清單的圖案跟顯示與否 抓出來 並存入使用者收藏的地點中
       let select_list_icon_and_appear = await dao_map.select_2("user_map_list","user_name",user_list_data.user_name,"list_name",user_list_data.list_name)
 
-      console.log(select_list_icon_and_appear);
+      //console.log(select_list_icon_and_appear);
 
       user_list_data.list_icon = select_list_icon_and_appear[0].list_icon;
       user_list_data.appear_list = select_list_icon_and_appear[0].appear_list;
+
+      //把欲存的地點的經緯度及資訊抓出來 並存入使用者收藏的地點中
+      let select_map_place = await dao_map.select("map","name",user_list_data.place_name);
+
+      user_list_data.latitude = select_map_place[0].latitude;
+      user_list_data.longitude = select_map_place[0].longitude;
+      user_list_data.information = select_map_place[0].information;
+
+      //console.log(user_list_data)
 
       let insert_user_place_data = await dao_map.insert("user_map_place",user_list_data,user_list_data.user_name);
 
@@ -471,8 +476,55 @@ app.post("/api/user/map_list/place",async function(req,res){
         place_in_this_list:select_user_last_place,
       }
 
-      console.log(select_user_last_place)
+      //console.log(select_user_last_place)
       res.send(select_user_last_place);
+
+
+
+
+      // let user_list_data = {
+      //   user_name : select_user_result[0].name,
+      //   list_name : list_data.data.list_name,
+      //   place_name :list_data.data.place_name,
+      //   place_order :list_data.data.place_order,
+      //   longitude : list_data.data.longitude,
+      //   latitude : list_data.data.latitude,
+      //   information :list_data.data.information
+      // };
+
+      // console.log(user_list_data)
+      
+      // let select_user_insert_place = await dao_map.select_3("user_map_place","user_name",user_list_data.user_name,"place_name",user_list_data.place_name,"list_name",user_list_data.list_name);
+      
+      // console.log(select_user_insert_place)
+      
+      // if(select_user_insert_place.length >0){
+      //   var error = {
+      //     "error": "此地點已收藏"
+      //   };
+      //   res.send(error);
+      //   return;
+      // }
+
+      // let select_list_icon_and_appear = await dao_map.select_2("user_map_list","user_name",user_list_data.user_name,"list_name",user_list_data.list_name)
+
+      // console.log(select_list_icon_and_appear);
+
+      // user_list_data.list_icon = select_list_icon_and_appear[0].list_icon;
+      // user_list_data.appear_list = select_list_icon_and_appear[0].appear_list;
+
+      // let insert_user_place_data = await dao_map.insert("user_map_place",user_list_data,user_list_data.user_name);
+
+      // let last_place_id = await dao_map.select_last_insert_id()
+
+      // let select_user_last_place = await dao_map.select("user_map_place","No",last_place_id[0]["LAST_INSERT_ID()"]);
+
+      // let data={
+      //   place_in_this_list:select_user_last_place,
+      // }
+
+      // console.log(select_user_last_place)
+      // res.send(select_user_last_place);
 
 
     }else if(list_data.type == "select"){
